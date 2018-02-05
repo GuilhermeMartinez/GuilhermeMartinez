52f3775e12df06699d97450c187fee69bfb5eb81
guilherme.mpereira6 <guilherme.mpereira6@OSA0557592W10-1>
2018-02-05 14:16:33 -0200

    TesteDrive

---
+++ CalcGuilherme_/CalcGuilherme_/CalcGuilherme_/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using CalcGuilherme_.ViewModel;
+
+namespace CalcGuilherme_
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+            BindingContext = new ViewModel1();
+		}
+	}
+}
